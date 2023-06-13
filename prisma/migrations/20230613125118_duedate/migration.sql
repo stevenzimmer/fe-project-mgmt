245d95ecda2c6dc547c7c830ebbdc88efb5e0b03
Steven Zimmer <webdevzim@gmail.com>
2023-06-13 05:59:51 -0700

    db seeded

---
+++ prisma/migrations/20230613125118_duedate/migration.sql
@@ -0,0 +1,9 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `dueDate` on the `Project` table. All the data in the column will be lost.
+
+*/
+-- AlterTable
+ALTER TABLE "Project" DROP COLUMN "dueDate",
+ADD COLUMN     "due" TIMESTAMP(3);
